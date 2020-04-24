b2e0a5f783d26ddf7d0cf0680ea3beb6a53c388e
PTRIGGS1775 <PT.RIGGS1775@gmail.com>
2020-04-23 20:46:30 -0700

    added helloworld

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+def add_1(x, y, z=10):
+    assert type(x) == int, 'Get your shit together, thats not a number!'
+    x = x + 1
+    y = y + 1
+    z = z + 1
+    return(x, y, z)
+
+x = int(input('Enter value for x: '))
+y = int(input('Enter value for y: '))
+add = add_1(x,y)
+print(add)
+print(add_1)
