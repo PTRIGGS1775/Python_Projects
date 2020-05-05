2f3a9683d5261e89ddcb1647d3c0206da4a6ba6f
PTRIGGS1775 <PT.RIGGS1775@gmail.com>
2020-05-04 19:40:05 -0700

    Updated with lists

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
-def add_1(x, y, z=10):
+#This code was used to test my function and input skills.
+'''def add_1(x, y, z=10):
     assert type(x) == int, 'Get your shit together, thats not a number!'
     x = x + 1
     y = y + 1
@@ -10,4 +11,11 @@ y = int(input('Enter value for y: '))
 add = add_1(x,y)
 print(add)
 print(add_1)
-print("we've made a change to the document")
+print("we've made a change to the document")'''
+
+#This function is for testing lists.
+empty_list = []
+print(empty_list)
+value1 = input('What groceries do we need? ')
+empty_list.append(value1)
+print(empty_list)
